7388c2e7d155e17a9123297fee8b52eadc1377d2
Javad PourMostafa <javad.pourmostafa@gmail.com>
2018-11-26 22:49:42 +0330

    Add setup files

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stopwords_guilannlp",
-    version=__import__("stopwords_guilannlp").stopword_version() + "." + "2.5",
+    version=__import__("stopwords_guilannlp").stopword_version() + "." + "2.6",
     author="Javad PourMostafa",
     author_email="javad.pourmostafa@gmail.com",
     description="A comprehensive package for stopwords in NLP and text mining",
@@ -13,6 +13,12 @@ setuptools.setup(
     long_description=open('README.md').read(),
     url="https://github.com/joyebright/stopwords_guilannlp",
     packages=setuptools.find_packages(),
+    zip_safe=False,
+    package_data={
+        'stopwords-guilannlp': [
+            'stopwords-guilannlp/Languages/*.csv'
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
